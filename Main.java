c839e1e12d56903064c3f9b66c17ce44806b42ad
rovmont <rovin_montano@dlsu.edu.ph>
2024-07-30 23:34:09 +0800

    Added in-line comments. Created new Main Menu model class.

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-       {
-            HotelReservationSystem model = new HotelReservationSystem();
-            MainMenuView view = new MainMenuView();
-            MainMenuController controller = new MainMenuController(view, model);
-            view.setVisible(true);
-        };
+        // Create an instance of HotelReservationSystem
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+
+        // Create an instance of MainMenuModel with the hotelReservationSystem
+        MainMenuModel model = new MainMenuModel(hotelReservationSystem);
+
+        // Create an instance of MainMenuView
+        MainMenuView view = new MainMenuView();
+
+        // Create an instance of MainMenuController with the view and model
+        MainMenuController controller = new MainMenuController(view, model);
+
+        // Make the view visible
+        view.setVisible(true);
     }
 }
