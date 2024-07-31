8b7b9c7ec7636f19db01e7b687a20454cdea71ed
rovmont <rovin_montano@dlsu.edu.ph>
2024-07-31 10:11:58 +0800

    Updated methods to accommodate new GUI

--- Hotel.java
+++ Hotel.java
@@ -74,7 +74,15 @@ public class Hotel {
      *
      * @return true if rooms were successfully added, false otherwise.
      */
-    public boolean addRoom(int nRoomsToCreate, Room.RoomType type) {
+    /**
+     * Adds a specified number of rooms to the hotel with the given room type.
+     *
+     * @param nRoomsToCreate The number of rooms to create.
+     * @param type           The type of rooms to create.
+     *
+     * @return true if rooms were successfully added, false otherwise.
+     */
+    public boolean addRooms(int nRoomsToCreate, Room.RoomType type) {
         int existingRooms = roomsList.size();
         int maxRooms = 50;
 
@@ -86,7 +94,9 @@ public class Hotel {
                 int roomOnFloor = roomNumber % 10 == 0 ? 10 : roomNumber % 10;
                 int sum = floorNumber * 100 + roomOnFloor;
 
-                String roomName = Integer.toString(sum);
+                // Generate room name with the first letter of the room type
+                String roomTypeInitial = type.name().charAt(0) + ""; // Get first letter of the room type
+                String roomName = Integer.toString(sum) + roomTypeInitial; // Append the room type initial
                 roomsList.add(new Room(roomName, type));
             }
             return true; // Rooms successfully added
@@ -94,6 +104,7 @@ public class Hotel {
 
         return false; // Invalid number of rooms to create
     }
+
 
     /**
      * Removes a room from the hotel if it has no reservations.
--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -32,7 +32,13 @@ public class HotelReservationSystem {
             return false;
         }
 
-        if (!hotelName.matches("^[a-zA-Z][a-zA-Z0-9]*$")) {
+        // Check if the hotel name starts with a number
+        if (Character.isDigit(hotelName.trim().charAt(0))) {
+            return false;
+        }
+
+        // Allow alphanumeric characters and dashes with whitespace
+        if (!hotelName.matches("^[a-zA-Z0-9-\\s]+$")) {
             return false;
         }
 
@@ -62,7 +68,13 @@ public class HotelReservationSystem {
             return false;
         }
 
-        if (!newHotelName.matches(".*[a-zA-Z].*")) {
+        // Check if the new hotel name starts with a number
+        if (Character.isDigit(newHotelName.trim().charAt(0))) {
+            return false;
+        }
+
+        // Allow alphanumeric characters and dashes with whitespace
+        if (!newHotelName.matches("^[a-zA-Z0-9-\\s]+$")) {
             return false;
         }
 
@@ -80,7 +92,7 @@ public class HotelReservationSystem {
      * @return true if rooms were successfully added, false otherwise.
      */
     public boolean addRoomsToHotel(Hotel hotel, int nRoomsToCreate, Room.RoomType type) {
-        return hotel.addRoom(nRoomsToCreate, type);
+        return hotel.addRooms(nRoomsToCreate, type);
     }
 
     /**
@@ -122,124 +134,56 @@ public class HotelReservationSystem {
     }
 
     /**
-     * Displays the details of a specified hotel.
+     * Returns a list of rooms of a specific type in a hotel.
      *
-     * @param hotel The hotel whose details are to be displayed.
+     * @param hotel The hotel to get the rooms from.
+     * @param type  The type of rooms to filter by.
+     * @return A list of rooms of the specified type.
      */
-    public void viewHotel(Hotel hotel) {
-        System.out.println("Name of hotel: " + hotel.getName());
-        System.out.println("Base price of rooms: " + hotel.getRoomsList().get(0).getBasePrice());
-        System.out.println("Total number of rooms: " + hotel.getRoomsList().size());
-        System.out.println("Total number of reservations: " + hotel.getReservationsList().size());
-        System.out.println("Total number of available rooms: " + hotel.getAvailableRoomsCount());
-        System.out.println("Estimated earnings: " + (hotel.getRoomsList().get(0).getBasePrice() * 30 * hotel.getRoomsList().size()));            // changed from 31 to 30 since nights are being counted
-        System.out.println("Actual earnings: " + hotel.getActualEarnings());
-    }
-
-    /**
-     * Displays the rooms available for booking within a specified date range.
-     *
-     * @param hotel        The hotel whose rooms are to be checked.
-     * @param checkInDate  The check-in date.
-     * @param checkOutDate The check-out date.
-     */
-    public void viewRoomsByDate(Hotel hotel, int checkInDate, int checkOutDate) {
-        int unavailableCount = 0;
-        int availableCount = 0;
-
-        System.out.println("\nThe rooms available on the specified check-in and check-out dates are: ");
-        for (int i = 0; i < hotel.getRoomsList().size(); i++) {
-            Room room = hotel.getRoomsList().get(i);
-            if (room.isAvailableToReserve(checkInDate, checkOutDate)) {
-                availableCount++;
-                System.out.println("\t" + availableCount + ". Room " + room.getName());
-            } else {
-                unavailableCount++;
+    public ArrayList<Room> getRoomsByType(Hotel hotel, Room.RoomType type) {
+        ArrayList<Room> roomsOfType = new ArrayList<>();
+        for (Room room : hotel.getRoomsList()) {
+            if (room.getType() == type) {
+                roomsOfType.add(room);
             }
         }
-
-        if (availableCount == 0) {
-            System.out.println("No rooms available for booking on the specified dates.\n");
-        } else {
-            System.out.println(" ");
-        }
-
-        if (unavailableCount == 0) {
-            System.out.println("All rooms are available for booking.\n");
-        } else {
-            System.out.println("The rooms unavailable on the specified check-in and check-out dates are: ");
-            int unavailableRoomIndex = 0;
-            for (int i = 0; i < hotel.getRoomsList().size(); i++) {
-                Room room = hotel.getRoomsList().get(i);
-                if (!room.isAvailableToReserve(checkInDate, checkOutDate)) {
-                    unavailableRoomIndex++;
-                    System.out.println("\t" + unavailableRoomIndex + ". Room " + room.getName() + "\n");
-                }
-            }
-        }
+        return roomsOfType;
     }
 
     /**
-     * Displays detailed information about a specific room.
+     * Returns a list of available rooms for reservation within a specified date range.
      *
-     * @param room The room whose details are to be displayed.
-     */
-    public void viewRoomInfo(Room room) {
-        int counter = 0;
-        System.out.println("Room number: " + room.getName());
-        System.out.println("Price per night: " + room.getBasePrice());
-        System.out.println("Status: " + room.getStatus() + "\n");
-
-        if (!room.getReservationsList().isEmpty()) {
-            System.out.println("The room is currently booked on the following dates: ");
-            for (Reservation reservation : room.getReservationsList()) {
-                counter++;
-                System.out.println("\tReservation " + counter + ": " + reservation.getCheckInDate() + " - "
-                        + reservation.getCheckOutDate());
-            }
-            System.out.println(" ");
-        } else {
-            System.out.println("The room currently has no reservations.\n");
-        }
-    }
-
-    /**
-     * Displays detailed information about a specific reservation.
-     *
-     * @param reservation The reservation whose details are to be displayed.
-     */
-    public void viewReservationInfo(Reservation reservation) {
-        int totalDays = reservation.getCheckOutDate() - reservation.getCheckInDate();
-
-        System.out.println("Room number: " + reservation.getRoom().getName());
-        System.out.println("Guest name: " + reservation.getGuestName());
-        System.out.println("Reservation ID: " + reservation.getReservationID());
-        System.out.println("Check-in date: Day " + reservation.getCheckInDate());
-        System.out.println("Check-out date: Day " + reservation.getCheckOutDate());
-        System.out.println("Total price: " + reservation.getTotalPrice());
-        System.out.println("Price Breakdown: (" + reservation.getRoom().getBasePrice() + " per night) * " + totalDays
-                + " = " + reservation.getTotalPrice());
-    }
-
-    /**
-     * Returns a list of rooms available for reservation within a specified date
-     * range.
-     *
-     * @param hotel        The hotel whose rooms are to be checked.
+     * @param hotel        The hotel to check for available rooms.
      * @param checkInDate  The check-in date.
      * @param checkOutDate The check-out date.
      * @return A list of available rooms.
      */
-    public ArrayList<Room> availableRooms(Hotel hotel, int checkInDate, int checkOutDate) {
+    public ArrayList<Room> getAvailableRooms(Hotel hotel, int checkInDate, int checkOutDate) {
         ArrayList<Room> availableRooms = new ArrayList<>();
-
         for (Room room : hotel.getRoomsList()) {
             if (room.isAvailableToReserve(checkInDate, checkOutDate)) {
                 availableRooms.add(room);
             }
         }
-
         return availableRooms;
+    }
+
+    /**
+     * Returns a list of unavailable rooms for reservation within a specified date range.
+     *
+     * @param hotel        The hotel to check for unavailable rooms.
+     * @param checkInDate  The check-in date.
+     * @param checkOutDate The check-out date.
+     * @return A list of unavailable rooms.
+     */
+    public ArrayList<Room> getUnavailableRooms(Hotel hotel, int checkInDate, int checkOutDate) {
+        ArrayList<Room> unavailableRooms = new ArrayList<>();
+        for (Room room : hotel.getRoomsList()) {
+            if (!room.isAvailableToReserve(checkInDate, checkOutDate)) {
+                unavailableRooms.add(room);
+            }
+        }
+        return unavailableRooms;
     }
 
     /**
